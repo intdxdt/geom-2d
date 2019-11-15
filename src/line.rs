df8967a289654963dbc929cb8f293fcd92a1bd71
Titus Tienaah <titus.intdxdt@gmail.com>
2019-11-15 23:41:42 +0000

    WIP: line

--- src/line.rs
+++ src/line.rs
@@ -1,8 +1,14 @@
 use rtree_2d::RTree;
-use crate::{Point};
+use crate::Point;
 use crate::MonoMBR;
+use bbox_2d::MBR;
 use rstar::primitives::Line;
+use serde_json::value::Value::Null;
 
+const MINI_MONO_SIZE: usize = 8;
+const NULL: i32 = -9;
+
+#[derive(Clone, Debug)]
 struct LineString {
     coordinates: Vec<Point>,
     bbox: MonoMBR,
@@ -11,7 +17,43 @@ struct LineString {
 }
 
 impl LineString {
-    fn new() {
+    fn new(coordinates: &[Point]) -> LineString {
+        let mut coords = Vec::with_capacity(coordinates.len());
+        coords.extend_from_slice(coordinates);
+        if coordinates.len() < 2 {
+            panic!("a linestring must have at least 2 coordinates");
+        }
+        let mut ln = LineString {
+            coordinates: coords,
+            bbox: MonoMBR::new_default(),
+            chains: Vec::new(),
+            index: RTree::new(),
+        };
+        ln
+    }
 
-	}
+    fn process_chains(&mut self) -> &mut Self {
+        let n = self.coordinates.len();
+        let (i, j) = (0, n - 1);
+        let mut a = self.coordinates[i];
+        let mut bbox: MBR = a.as_array().into();
+
+        if n < MINI_MONO_SIZE {
+            for pt in self.coordinates.iter() {
+                bbox.expand_to_include_xy(pt.x, pt.y);
+            }
+            self.bbox = MonoMBR::new_mono(bbox);
+            self.bbox.i = 0;
+            self.bbox.j = n - 1;
+            self.chains.push(self.bbox);
+            return self;
+        }
+        let mono_limit = ((j + 1) as f64 + 1.0).log2() as i32;
+
+        let (prev_x, prev_y) = (NULL, NULL);
+        self.bbox = MonoMBR::new_mono(bbox);
+
+
+        self
+    }
 }
--- src/mono.rs
+++ src/mono.rs
@@ -2,15 +2,28 @@ use rstar::{AABB, RTreeObject, PointDistance};
 use bbox_2d::MBR;
 use crate::Point;
 
+#[derive(Copy, Clone, Debug)]
 pub struct MonoMBR {
-    mbr: MBR,
-    i: usize,
-    j: usize,
+    pub mbr: MBR,
+    pub i: usize,
+    pub j: usize,
 }
 
 impl MonoMBR {
     pub fn new(a: Point, b: Point, i: usize, j: usize) -> MonoMBR {
-        MonoMBR { mbr: MBR::new_from_bounds(a.as_array(),  b.as_array()), i, j }
+        MonoMBR { mbr: MBR::new_from_bounds(a.as_array(), b.as_array()), i, j }
+    }
+
+    pub fn new_mono(mbr: MBR) -> MonoMBR {
+        MonoMBR { mbr, i: 0, j: 0 }
+    }
+
+    pub fn new_default() -> MonoMBR {
+        MonoMBR { mbr: MBR::new_default(), i: 0, j: 0 }
+    }
+
+    pub fn bbox(&self, other: &Self) -> &MBR {
+        &self.mbr
     }
 
     pub fn equals(&self, other: &Self) -> bool {
